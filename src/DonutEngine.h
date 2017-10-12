2530d4c56da7c96c99761f701d3e8cfb698a24e2
Aaron Roodman <roodman@slac.stanford.edu>
2017-10-12 10:05:13 -0700

    remove x,yDESI just use x,yDECam

--- src/DonutEngine.h
+++ src/DonutEngine.h
@@ -65,7 +65,7 @@ public:
   void getParCurrent(double** ARGOUTVIEW_ARRAY1, int* DIM1);  
   void getDerivatives(double** ARGOUTVIEW_ARRAY1, int* DIM1);  
   void setXYDECam(double x, double y){_xDECam = x; _yDECam = y;};
-  void setXYDESI(double thetax, double thetay){_xDESI=thetax;  _yDESI=thetay;}; // added to handle DESI's geometric differences from DECam
+  //void setXYDESI(double thetax, double thetay){_xDESI=thetax;  _yDESI=thetay;}; // added to handle DESI's geometric differences from DECam
   void fillPar(double* par, int n);
   void calcWFMtoImage(double* IN_ARRAY2, int DIM1, int DIM2);
 
@@ -127,8 +127,8 @@ public:
   int nCallsCalcAll,nCallsCalcDerivative;
   int nZernikeSize;
   
-  Real _xDESI;
-  Real _yDESI;
+  //  Real _xDESI;
+  //  Real _yDESI;
 
 protected:
   
