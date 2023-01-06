5f19361fc7cfe6560803b3632490768601125983
aaronroodman <roodman@cent7b.slac.stanford.edu>
2023-01-06 11:37:50 -0800

    Consolidate test scripts on data donuts, add more sample donuts'

--- test/test-data-donuts.py
+++ test/test-data-donuts.py
@@ -1,6 +1,5 @@
 ###
-### Fit one DECam donut with 3 method, first just Zernike polynomials, 
-### next with high order Zernikes from Zemax, last with pupil grid from BigDonuts
+### Fit 4 donuts with Zernike polynomials and high order Zernikes from Zemax and pupil grid from Big Donuts
 ###
 
 import os
@@ -8,22 +7,15 @@ import numpy as np
 import pandas as pd
 from astropy.io import fits
 from tabulate import tabulate
-
-from donutlib.makedonut import makedonut
 from donutlib.donutfit import donutfit
 from donutlib.wavefrontmap import wavefrontmap
-from donutlib.decamutil import decaminfo
 
-decamInfo = decaminfo()
 
-wmap = os.path.expanduser("input/decam_2012-nominalzernike.pickle")
-wmapobj = wavefrontmap(wmap)
+infiles = ['input/DECam_00284696.S4.0008.stamp.fits','input/DECam_00284696.S25.0001.stamp.fits',
+           'input/DECam_00345461.S4.0018.stamp.fits','input/DECam_00345461.S25.0018.stamp.fits',
+            'input/DECam_00153648.S4.0010.stamp.fits','input/DECam_00153648.S25.0019.stamp.fits']
 
-inprefix = 'input/DECam_00284692.S25.0020'
-outprefix = 'output/DECam_00284692-fit%d.S25.0020'
-infiles = [inprefix+'.stamp.fits']
-
-# fit the donut three ways, just low order Zernikes, with wavefront map for high order Zernikes, and also with pupil grid
+filenames = ['out'+afile[2:11]+'%s'+afile[11:-11]+'.second.donut.fits' for afile in infiles]
 
 def fitdonut1():
 
@@ -33,7 +25,8 @@ def fitdonut1():
                    "gain":4.5,"printLevel":1}
 
     df = donutfit(**fitinitDict)
-    outfiles = [outprefix % (1)]
+
+    outfiles = ['out'+afile[2:-11] for afile in infiles]
 
     # fit dictionary
     fitDict  = {}
@@ -47,6 +40,9 @@ def fitdonut1():
 
 def fitdonut2():
 
+    wmap = os.path.expanduser("input/decam_2012-nominalzernike.pickle")
+    wmapobj = wavefrontmap(wmap)
+
     fitinitDict = {"nZernikeTerms":37,
         "fixedParamArray1":[0,1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
         "fixedParamArray2":[0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
@@ -55,8 +51,8 @@ def fitdonut2():
         "gain":4.5,"printLevel":1,"wavefrontMap":wmapobj}
 
     df = donutfit(**fitinitDict)
-    outfiles = [outprefix % (2)]
 
+    outfiles = ['out'+afile[2:11]+'_wmap'+afile[11:-11] for afile in infiles]
 
     # fit dictionary
     fitDict  = {}
@@ -66,9 +62,12 @@ def fitdonut2():
     for i in range(len(infiles)):
         fitDict["inputFile"] = infiles[i]
         fitDict["outputPrefix"] = outfiles[i]
-        df.setupFit(**fitDict)
+        df.setupFit(**fitDict)        
 
 def fitdonut3():
+
+    wmap = os.path.expanduser("input/decam_2012-nominalzernike.pickle")
+    wmapobj = wavefrontmap(wmap)
 
     fitinitDict = {"nZernikeTerms":37,
         "fixedParamArray1":[0,1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
@@ -78,9 +77,9 @@ def fitdonut3():
         "gain":4.5,"printLevel":1,"wavefrontMap":wmapobj,
         "deltaWFM":'input/DECam_236392_finegrid512.fits'}
 
-
     df = donutfit(**fitinitDict)
-    outfiles = [outprefix % (3)]
+
+    outfiles = ['out'+afile[2:11]+'_pupilgrid'+afile[11:-11] for afile in infiles]
 
     # fit dictionary
     fitDict  = {}
@@ -92,25 +91,33 @@ def fitdonut3():
         fitDict["outputPrefix"] = outfiles[i]
         df.setupFit(**fitDict)
 
+
 def anafits():
-    outpostfix = '.second.donut.fits'
-    files = [outprefix % (1)+outpostfix,outprefix % (2)+outpostfix,
-outprefix % (3)+outpostfix]
+
+    fitnames = ["","_wmap","_pupilgrid"]
 
     keys = ['CHI2','NELE','RZERO','BKGD']
     for iZ in range(4,15):
         keys.append('ZERN%d' % (iZ))
 
     outdict = {}
+    outdict['FITNAME'] = []
+    outdict['FILENAME'] = []
+
     for key in keys:
         outdict[key] = []
 
-    for j,afile in enumerate(files):
-        hdu = fits.open(afile)
-        headd = hdu[0].header
+    for j,afile in enumerate(filenames):
+        for k,afit in enumerate(fitnames):
 
-        for i,key in enumerate(keys):
-            outdict[key].append(headd[key])
+            thefile = afile % (afit)
+            hdu = fits.open(thefile)
+            headd = hdu[0].header
+            outdict['FITNAME'].append(afit)
+            outdict['FILENAME'].append(afile[15:33])
+
+            for i,key in enumerate(keys):
+                outdict[key].append(headd[key])
 
     df = pd.DataFrame(outdict)
     print(tabulate(df, headers = 'keys',floatfmt='.3f'))
@@ -121,4 +128,3 @@ if __name__ == '__main__':
     fitdonut2()
     fitdonut3()
     anafits()
-
